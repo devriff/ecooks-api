a9983cb0a7d24ab2c6298f77292396e7b498b681
TheDevShredder <ragdev357@gmail.com>
2018-01-19 13:09:27 +0800

    change message in api rest

--- application/controllers/Recipes.php
+++ application/controllers/Recipes.php
@@ -7,28 +7,39 @@ class Recipes extends BaseController {
     if ($category_id) {
       $result = $this->Recipe->_get_recipes($category_id);
       if ($result) {
-        $data['status'] = true;
+        $data['success'] = true;
         $data['data'] = $result;
+        echo json_encode($data);
+      } else {
+        $data['success'] = false;
+        $data['data'] = $result;
+        $data['message'] = 'No results found.';
         echo json_encode($data);
       }
     } else {
-      $keywords = $this->input->get('keywords');
-      if (isset($keywords)) {
-        $result = $this->Recipe->_search_recipes($keywords);
+      $keyword = $this->input->get('keyword');
+      if (isset($keyword)) {
+        $result = $this->Recipe->_search_recipes($keyword);
         if ($result) {
-          $data['status'] = true;
+          $data['success'] = true;
           $data['data'] = $result;
           echo json_encode($data);
         } else {
-          $data['status'] = false;
+          $data['success'] = false;
           $data['data'] = $result;
+          $data['message'] = 'No results found.';
           echo json_encode($data);
         }
       } else {
         $result = $this->Recipe->_get_recipes();
         if ($result) {
-          $data['status'] = true;
+          $data['success'] = true;
           $data['data'] = $result;
+          echo json_encode($data);
+        } else {
+          $data['success'] = false;
+          $data['data'] = $result;
+          $data['message'] = 'No results found.';
           echo json_encode($data);
         }
       }
@@ -38,7 +49,7 @@ class Recipes extends BaseController {
   public function get_recipe ($recipe_id = false) {
     $result = $this->Recipe->_get_recipe($recipe_id);
     if ($result) {
-      $data['status'] = true;
+      $data['success'] = true;
       $data['data'] = $result;
       echo json_encode($data);
     }
